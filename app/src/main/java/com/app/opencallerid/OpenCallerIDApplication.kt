2693f1235d2c71e707300672bdcc3191a84be354
mlm-studios <you@example.com>
2025-04-25 13:39:41 +0530

    Missed some template code

---
+++ app/src/main/java/com/app/opencallerid/OpenCallerIDApplication.kt
@@ -0,0 +1,10 @@
+package com.app.opencallerid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OpenCallerIDApplication : Application() {
+    // Usually, this class can remain empty for basic Hilt setup.
+    // You might add custom initialization logic in onCreate() later if needed.
+}
